a43000bfc90c4726694e34601f5118650a7688d7
hongduck710 <hongik710@gmail.com>
2024-10-21 21:03:16 +0900

    1:47:24까지 완료. npm i -D json-server 설치

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
+
 import { NavLink } from "react-router-dom";
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive 
+    ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+    : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
     <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -15,20 +21,15 @@ const Navbar = () => {
           <div className='md:ml-auto'>
             <div className='flex space-x-2'>
                 <NavLink to="/"
-                className={({ isActive} ) => isActive ? `bg-black 
-                text-white hover:bg-gray-900 hover:text-white 
-                rounded-md px-3 py-2` : `text-white hover:bg-gray-900
-                hover:text-white rounded-md px-3 py-2`}>
+                className={linkClass}>
                     Home
                 </NavLink>
                 <NavLink to="/jobs"
-                className="text-white bg-black hover:bg-gray-900
-                hover:text-white rounded-md px-3 py-2">
+                className={linkClass}>
                     Jobs
                 </NavLink>
                 <NavLink to="/add-job"
-                className="text-white bg-black hover:bg-gray-900
-                hover:text-white rounded-md px-3 py-2">
+                className={linkClass}>
                     Add Job
                 </NavLink>               
             </div>
