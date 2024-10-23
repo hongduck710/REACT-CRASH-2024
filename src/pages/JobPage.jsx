cb910e3f63aa05533814e9b343c77ed76757ba74
hongduck710 <hongik710@gmail.com>
2024-10-23 21:55:34 +0900

    2:19:48까지 완료. JobPage 데이터 출력 진행중

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,26 +1,47 @@
-import { useState, useEffect } from 'react';
+import {  useParams, useLoaderData, Link } from 'react-router-dom';
+import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 
 const JobPage = () => {
-    const [job, setJob] = useState(null);
-    
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(apiUrl);
-                const data = await res.json();
-                setJobs(data);
-            } catch(error) {
-                console.log('Error fetching data', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchJob();
-    }, []);
-    
-    return <div>JobPage</div>;
+    const { id } = useParams();
+    const job = useLoaderData();
+
+    return (
+        <>
+            <section>
+                <div className='container m-auto py-6 px-6'>
+                    <Link 
+                        to='/jobs'
+                        className='text-indigo-500 hover:text-indigo-600 flex items-center'>
+                        <FaArrowLeft className='mr-2' />
+                    </Link>    
+                </div>
+            </section>
+            <section className='bg-indigo-50'>
+                <div className='container m-auto py-10 px-6'>
+                    <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+                        <main>
+                            <div className='bg-white p-6 rounded-lg shadow-md
+                            text-center md:text-left'>
+                                <div className='text-gray-500 mb-4'>{ job.type }</div>
+                                <h1 className='text-3xl font-bold mb-4'>{ job.title }</h1>
+                                <div className='text-gray-500 mb-4 flex items-center
+                                justify-center md:justify-start'>
+                                    <FaMapMarker className='text-orange-700 mr-1'/>
+                                    <p className='text-orange-700'>{ job.location }</p>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
-const jobLoader = async ({});
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+}
 
-export default JobPage;
+export  { JobPage as default, jobLoader };
