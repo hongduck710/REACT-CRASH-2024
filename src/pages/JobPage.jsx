dd36640152d4f3a321ad5130fe421184504367fb
hongduck710 <hongik710@gmail.com>
2024-10-30 21:53:21 +0900

    2:45:41 까지 완료. 삭제 기능 구현 중

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,17 @@
-import {  useParams, useLoaderData, Link } from 'react-router-dom';
+import {  useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const job = useLoaderData();
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm("Are you sure you want to delete this listing?");
+        if(!confirm) return;
+        deleteJob(jobId);
+        navigate("/jobs");
+    }
 
     return (
         <>
@@ -52,13 +60,15 @@ const JobPage = () => {
                                     { job.company.contactEmail }
                                 </p>
                                 <h3 className='text-xl'>Contact Phone:</h3>
-                                <p className='my-2 bg-indigo-100 p-2 font-bold'>{ job.company.contactPhone }</p>
+                                <p className='my-2 bg-indigo-100 p-2 font-bold'>
+                                    { job.company.contactPhone }
+                                </p>
                             </div>
                             <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
                                 <Link to={ `/jobs/edit/${job.id}` } className='bg-indigo-500 hover:bg-indigo-600 text-white
                                 text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none
                                 focus:shadow-outline mt-4 block'>Edit Job</Link>
-                                <button className='bg-red-500 hover:bg-red-600 text-white
+                                <button onClick={ () => onDeleteClick(job.id) } className='bg-red-500 hover:bg-red-600 text-white
                                 font-bold py-2 px-4 rounded-full w-full focus:outline-none 
                                 focus:shadow-outline mt-4 block'>Delete Job</button>
                             </div>
