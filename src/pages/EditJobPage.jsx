4bdb90f0b2b2357dc5a1a29e134df65bae82669f
hongduck710 <hongik710@gmail.com>
2024-11-11 21:06:31 +0900

    수정 까지 완료. npm run build,  npm run preview까지 실행. 완성!

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,19 +1,48 @@
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
-const EditJobPage = () => {
-    const job = useLoaderData();
-    const [title, setTitle] = useState(job.title);
-    const [type, setType] = useState(job.type);
-    const [location, setLocation] = useState(job.location);
-    const [description, setDescription] = useState(job.description);
-    const [salary, setSalary] = useState(job.salary);
-    const [companyName, setCompanyName] = useState(job.company.name);
-    const [companyDescription, setCompanyDescription] = useState(job.company.description);
-    const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
-    const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
+const EditJobPage = ({ updateJobSubmit }) => {
+  const job = useLoaderData();
+  const [title, setTitle] = useState(job.title);
+  const [type, setType] = useState(job.type);
+  const [location, setLocation] = useState(job.location);
+  const [description, setDescription] = useState(job.description);
+  const [salary, setSalary] = useState(job.salary);
+  const [companyName, setCompanyName] = useState(job.company.name);
+  const [companyDescription, setCompanyDescription] = useState(
+    job.company.description
+  );
+  const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+  const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
-    const submitForm = (e) => {};
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const updatedJob = {
+      id,
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone,
+      },
+    };
+
+    updateJobSubmit(updatedJob);
+
+    toast.success('Job Updated Successfully');
+
+    return navigate(`/jobs/${id}`);
+    };
 
     return (
         <section className="bg-indigo-50">
@@ -21,7 +50,7 @@ const EditJobPage = () => {
             <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md
             border m-4 md:m-0">
                 <form onSubmit={submitForm}>
-                    <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+                    <h2 className="text-3xl text-center font-semibold mb-6">Update Job</h2>
                     <div className="mb-4">
                         <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
                             Job Type
@@ -191,7 +220,7 @@ const EditJobPage = () => {
                             focus:shadow-outline"
                             type="submit"    
                         >
-                            Add Job
+                            Update Job
                         </button>
                     </div>
                 </form>
